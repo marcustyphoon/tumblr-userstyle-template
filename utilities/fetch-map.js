13c7716413ca07249e0aba6f7f1ec190472a36ef
Marcus <marcustyphoon@gmail.com>
2025-02-01 12:10:50 -0800

    Switch fetch script to Firefox

--- utilities/fetch-map.js
+++ utilities/fetch-map.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 import fs from 'node:fs/promises';
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({ browser: 'firefox' });
 const page = await browser.newPage();
 await page.goto('https://www.tumblr.com/');
 const data = await page.evaluate(async () => window.tumblr.getCssMap());
